493e2a01e9806b59d15030c46e733a28be1d2586
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-04-01 11:04:29 +0000

    DASH: apds9702x: fix maximum range, determined on yuga
    Change-Id: I4375228e3e755c88e15a344898d861b361425cc9

--- sensors/apds970x.c
+++ sensors/apds970x.c
@@ -59,7 +59,7 @@ static struct sensor_desc apds970x = {
 		.version = sizeof(sensors_event_t),
 		.handle = SENSOR_PROXIMITY_HANDLE,
 		.type = SENSOR_TYPE_PROXIMITY,
-		.maxRange = 1.0,
+		.maxRange = 10.0,
 		.resolution = 1.0,
 		.power = 2
 	},
